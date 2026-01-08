5a0751a89131423ca84c938936433468a508a189
TRIZONACCI <tristan.christiaens@outlook.com>
2026-01-08 02:35:59 -0500

    Update layout.tsx
    Add Google Tag Manager correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://apps.abacus.ai/chatllm/appllm-lib.js"></script>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TGL39GS9');`,
+          }}
+        />
+
+        {/* Abacus script (keep) */}
+        <script src="https://apps.abacus.ai/chatllm/appilm-lib.js"></script>
       </head>
+
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGL39GS9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
